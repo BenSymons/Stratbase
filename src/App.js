222970d83e38c1f1187270a1bf203acc982fd24a
BenSymons <bsymonsb@hotmail.co.uk>
2021-01-14 17:49:19 +0000

    import images and add map component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import './App.css';
+import Map from "./Components/map"; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Hello
+          Stratbase
         </p>
       </header>
+      <Map/>
     </div>
   );
 }
---
+++ src/Components/map.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Map = () => {
+    return (<img src={"../Images/FS1.png"} alt="First room of Forsaken City" />)
+}
+
+export default Map

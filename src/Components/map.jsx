1dce82cdc2cfba28c4269184b5bb0d22d9989826
BenSymons <bsymonsb@hotmail.co.uk>
2021-01-16 12:32:59 +0000

    add arrow buttons to map

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Map = () => {
-    return (<img src={"../Images/FS1.png"} alt="First room of Forsaken City" />)
+    const [level, setLevel] = useState("FS")
+    const [room, setRoom] = useState("1")
+
+    return (<div id="map">
+        <button className="up">&#8679;</button>
+        <div id="map-middle">
+            <button className="left">&#8678;</button>
+            <img className="roompic" src={`/Images/${level}${room}.png`} alt="First room of Forsaken City"></img>
+            <button className="right">&#8680;</button>
+        </div>
+        <button className="down">&#8681;</button>
+    </div>)
 }
 
 export default Map

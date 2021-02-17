b91653ac720bafd170d0f333b2e28c5e89937007
BenSymons <bsymonsb@hotmail.co.uk>
2021-02-17 16:38:16 +0000

    buttons can be used to change to the next room

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react"
+import exits from "../data/exits.json"
 
 const Map = () => {
     const [level, setLevel] = useState("FS")
     const [room, setRoom] = useState("1")
 
+    const move = (direction) => {
+        setRoom(exits[level][room][direction])
+    }
+
+    const isDisabled = (direction) => {
+        return (!exits[level][room].hasOwnProperty(direction))
+    }
+
     return (<div id="map">
-        <button className="up">&#8679;</button>
+        <button className="up" disabled={isDisabled("N")} onClick={() => move("N")}>&#8679;</button>
         <div id="map-middle">
-            <button className="left">&#8678;</button>
+            <button className="left" disabled={isDisabled("W")} onClick={() => move("W")}>&#8678;</button>
             <img className="roompic" src={`/Images/${level}${room}.png`} alt="First room of Forsaken City"></img>
-            <button className="right">&#8680;</button>
+            <button className="right" disabled={isDisabled("E")} onClick={() => move("E")}>&#8680;</button>
         </div>
-        <button className="down">&#8681;</button>
+        <button className="down" disabled={isDisabled("S")} onClick={() => move("S")}>&#8681;</button>
     </div>)
 }
 

83630099f2c78bb268e223ea1d9a37858af62438
BenSymons <bsymonsb@hotmail.co.uk>
2021-02-24 12:34:08 +0000

    add Golden Ridge exits and subchapters

--- src/Components/info.jsx
+++ src/Components/info.jsx
@@ -18,7 +18,8 @@ const info = (props) => {
     const bookmarks = {
         "FS": { "Start": "1-6", "Crossing": "7-14", "Chasm": "15-20" },
         "OS": { "Start": "1-12", "Intervention": "13-25", "Awake": "26-33" },
-        "CR": { "Start": "1-20", "Huge Mess": "21-45", "Elevator Shaft": "46-55", "Presidential Suite:": "56-64" }
+        "CR": { "Start": "1-20", "Huge Mess": "21-45", "Elevator Shaft": "46-55", "Presidential Suite:": "56-64" },
+        "GR": { "Start": "1-11", "Shrine": "12-16", "Old Trail": "17-24", "Cliff Face": "25-34" }
     }
 
     const subchapters = Object.keys(bookmarks[props.level])
@@ -55,7 +56,7 @@ const info = (props) => {
                 <button onClick={() => changeLevel("FS")}>Forsaken City</button>
                 <button onClick={() => changeLevel("OS")}>Old Site</button>
                 <button onClick={() => changeLevel("CR")}>Celestial Resort</button>
-                <button>Golden Ridge</button>
+                <button onClick={() => changeLevel("GR")}>Golden Ridge</button>
                 <button>Mirror Temple</button>
                 <button>Reflection</button>
                 <button>Summit</button>
